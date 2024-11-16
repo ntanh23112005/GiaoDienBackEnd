6f34349e920fbac12a6b2c1d4c24bee7c9132ea6
ntanh23112005 <anhntps38560@gmail.com>
2024-11-16 12:44:21 +0700

    theanh upload lab4

---
+++ Lab4/src/components/Bai3.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>Bài 3</h2>
+{{ greeting }}
+<button v-bind:title="buttonTittle" @click="changeCreeting">Click để biết giới tính mình cương</button>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+        greeting: 'Xin chào anh em!',
+        buttonTittle:'Click vào đây để biết giới tính minh cường'
+    };
+    },
+
+    methods:{
+        changeCreeting(){
+            this.greeting = 'Cày là gương',
+            this.buttonTittle= 'Bạn đã biết r đó';
+        }
+    }
+}
+</script>
---
+++ Lab4/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Bài 2</h2>
+  <h1>Chào mừng đến với VueJS</h1>
+</template>
+<style>
+h1{color: red;}
+</style>
